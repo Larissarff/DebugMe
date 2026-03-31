e3d01ee62a513a7dbbc6de8d1edfabdf0d3abe13
Larissarff <larissa.rfferreira4800@gmail.com>
2026-03-31 09:57:43 -0300

    feat: Update Program.cs to configure DbContext and improve service registration

--- docs/src/DebugMeBackend/Program.cs
+++ docs/src/DebugMeBackend/Program.cs
@@ -1,12 +1,13 @@
+using DebugMeBackend.Data;
+using DebugMeBackend.Repositories;
+using DebugMeBackend.Repositories.Interfaces;
 using DebugMeBackend.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Controllers
 builder.Services.AddControllers();
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -21,27 +22,26 @@ builder.Services.AddSwaggerGen(options =>
     options.SwaggerDoc("v1", info);
 });
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddSingleton<UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<UserService>();
 
 WebApplication app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "DebugMe API v1");
-        options.RoutePrefix = "swagger"; 
+        options.RoutePrefix = "swagger";
     });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
