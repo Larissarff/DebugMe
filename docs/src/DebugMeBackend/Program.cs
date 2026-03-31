f374d6bdcd258ff242dc78cb41db76865513c7df
Larissarff <larissa.rfferreira4800@gmail.com>
2026-03-31 09:52:45 -0300

    feat: Configure Swagger for API documentation and add UserService to the service container

--- docs/src/DebugMeBackend/Program.cs
+++ docs/src/DebugMeBackend/Program.cs
@@ -1,41 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
+using DebugMeBackend.Services;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+// Controllers
+builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    OpenApiInfo info = new OpenApiInfo
+    {
+        Title = "DebugMe API",
+        Version = "v1",
+        Description = "API backend do projeto DebugMe"
+    };
+
+    options.SwaggerDoc("v1", info);
+});
+
+
+builder.Services.AddSingleton<UserService>();
+
+WebApplication app = builder.Build();
+
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "DebugMe API v1");
+        options.RoutePrefix = "swagger"; 
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
