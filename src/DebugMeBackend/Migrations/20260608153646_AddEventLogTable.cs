52f2404ae4543fa66b9328605fb7ba4d2351e36f
Larissarff <larissa.rfferreira4800@gmail.com>
2026-06-08 12:44:49 -0300

    feat: Add EventLog table with relationships to Emotion and User entities

---
+++ src/DebugMeBackend/Migrations/20260608153646_AddEventLogTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebugMeBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventLogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EmotionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Intensity = table.Column<int>(type: "INTEGER", nullable: false),
+                    EventDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventLogs_Emotions_EmotionId",
+                        column: x => x.EmotionId,
+                        principalTable: "Emotions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventLogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventLogs_EmotionId",
+                table: "EventLogs",
+                column: "EmotionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventLogs_UserId",
+                table: "EventLogs",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventLogs");
+        }
+    }
+}
